4a4b8ebdbd6e7c390ef72e4d55d99e1c3e45b36a
Max Shishkov <maxshishkov92@gmail.com>
2026-04-06 21:27:53 +0000

    add repl and tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MaxShishkov/pokedexcli
+
+go 1.26.1
---
+++ repl/repl_test.go
@@ -0,0 +1,47 @@
+package repl
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "   foo   bar   baz   ",
+			expected: []string{"foo", "bar", "baz"},
+		},
+		{
+			input:    "   singleword   ",
+			expected: []string{"singleword"},
+		},
+		{
+			input:    "      ",
+			expected: []string{},
+		},
+		{
+			input:    "Foo Bar BAZ",
+			expected: []string{"foo", "bar", "baz"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("cleanInput(%q) = %v; expected %v", c.input, actual, c.expected)
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("cleanInput(%q) = %v; expected %v", c.input, actual, c.expected)
+			}
+		}
+	}
+}
